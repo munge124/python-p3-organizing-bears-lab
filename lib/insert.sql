37a05a135b524387ddba3be7329ff3ac75286342
Charity <charitywanjiku8245@gmail.com>
2024-06-03 22:19:45 +0300

    Complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Mr. Chocolate",8,"M","brown","Composed",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Rowdy",6,"M","black","Aggressive",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Tabitha",7,"F","white","Composed",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Sergeant Brown",9,"M","brown","Composed",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Melissa",4,"F","white","Composed",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Grinch",3,"M","black","Aggressive",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Wendy",6,"F","brown","Composed",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES(NULL,7,"F","white","Aggressive",1);
